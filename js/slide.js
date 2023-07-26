c361af0cf5055b64b4857ba393b522f086330877
YasuiAyumi <138648164+YasuiAyumi@users.noreply.github.com>
2023-07-26 15:12:33 +0900

    hennkou
    uwa-

--- js/slide.js
+++ js/slide.js
@@ -1,7 +1,7 @@
 $(function(){//おまじない
 
   $('.slide').slick({
-    slidesToShow: 3,//一度に表示される数
+    slidesToShow: 1,//一度に表示される数
     slidesToScroll:1,//スライドするときの動きの単位
     autoplay: true,//自動スライド
     autoplaySpeed: 3000,//3000ミリ秒 == 3秒
